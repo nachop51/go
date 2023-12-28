6e528d41069cdb6dda7166e6f1d6676b474a8ff2
Nachop51 <nachoperalta0@gmail.com>
2023-12-28 19:13:11 -0300

    some progress

---
+++ live-reload/files.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func readFile(filename string) string {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(data)
+}
+
+func readDir(dirname string) []os.FileInfo {
+	// directories must be maps, and files must be slices
+
+	data, err := os.ReadDir(dirname)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var files []os.FileInfo
+
+	for _, file := range data {
+
+		fileInfo, err := file.Info()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		files = append(files, fileInfo)
+	}
+
+	return files
+}
+
+func updateFilesMap(filesMap map[string]os.FileInfo, dirname string) (map[string]os.FileInfo, bool) {
+	if filesMap == nil {
+		filesMap = make(map[string]os.FileInfo, 0)
+	}
+
+	var files = readDir(dirname)
+	var updated bool = false
+
+	for _, file := range files {
+		if file.IsDir() {
+			_, updated = updateFilesMap(filesMap, dirname+"/"+file.Name())
+			continue
+		}
+		if mapFile, ok := filesMap[dirname+"/"+file.Name()]; !ok {
+			updated = true
+			filesMap[dirname+"/"+file.Name()] = file
+		} else {
+			if mapFile.ModTime() != file.ModTime() {
+				updated = true
+				filesMap[dirname+"/"+file.Name()] = file
+			}
+		}
+	}
+
+	// fmt.Println(updated)
+
+	return filesMap, updated
+}
--- live-reload/main.go
+++ live-reload/main.go
@@ -2,97 +2,56 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
-	"log"
 	"os"
+	"os/exec"
 	"time"
 )
-
-func readFile(filename string) string {
-	data, err := os.ReadFile(filename)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(data)
-}
-
-func readDir(dirname string) []fs.FileInfo {
-
-	if dirname == "" {
-		dirname = "."
-	}
-
-	data, err := os.ReadDir(dirname)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var files []fs.FileInfo
-
-	for _, file := range data {
-
-		fileInfo, err := file.Info()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		files = append(files, fileInfo)
-	}
-
-	return files
-}
-
-func updateFilesMap(filesMap map[string]time.Time) map[string]time.Time {
-
-	if filesMap == nil {
-		filesMap = make(map[string]time.Time)
-	}
-
-	var files = readDir(".")
-
-	for _, file := range files {
-		value, ok := filesMap[file.Name()]
-
-		if ok && value.Equal(file.ModTime()) {
-			continue
-		} else if ok {
-			fmt.Println("File modified:", file.Name())
-		} else {
-			fmt.Println("New file:", file.Name())
-		}
-
-		filesMap[file.Name()] = file.ModTime()
-	}
-
-	return filesMap
-}
 
 func main() {
 	// var data string = readFile("data.txt")
 	// data := readFile("test.txt")
-
+	//
 	// fmt.Println(data)
-
+	//
 	// for _, file := range files {
 	// 	fmt.Println(file.IsDir(), file.Mode(), "'"+file.Name()+"'", "'"+fmt.Sprint(file.Size())+"'", file.ModTime())
 	// }
+	//
+	// var filesMap = make(map[string]time.Time)
+	// or just
+	// filesMap := map[string]time.Time{}
+	//
+	// for key, value := range filesMap {
+	// 	fmt.Println(key+":", value)
+	// }
+	// fmt.Printf("%p\n", filesMap)
 
-	var filesMap = updateFilesMap(nil)
+	var filesMap, updated = updateFilesMap(nil, ".")
 
-	for key, value := range filesMap {
-		fmt.Println(key+":", value)
-	}
-	fmt.Printf("%p\n", filesMap)
+	// fmt.Println(filesMap, updated)
 
 	for {
-		time.Sleep(1 * time.Second)
-		filesMap = updateFilesMap(filesMap)
-		fmt.Println(filesMap)
-		fmt.Printf("%p\n", filesMap)
-	}
+		if updated {
+			fmt.Println("Initializing...")
 
+			cmd := exec.Command("go", "run", ".")
+
+			cmd.Stdout = os.Stdout
+
+			err := cmd.Run()
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println("Done!")
+		}
+
+		time.Sleep(1 * time.Second)
+		filesMap, updated = updateFilesMap(filesMap, ".")
+	}
 }
