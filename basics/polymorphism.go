e148763e5264e4592abff5560bc31885d730ad1e
Nachop51 <nachoperalta0@gmail.com>
2024-06-11 23:19:18 -0300

    example

---
+++ basics/polymorphism.go
@@ -0,0 +1,28 @@
+package main
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Employee struct {
+	Person
+	Company string
+}
+
+func (p Person) PrintName() {
+	println(p.Name)
+}
+
+func test() {
+	emp := Employee{
+		Person: Person{
+			Name: "John",
+			Age:  30,
+		},
+	}
+
+	emp.PrintName()
+	emp.Person.PrintName()
+
+}
